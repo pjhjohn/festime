8f9618ebc7880f3368d5112d0f14079c9181e3f4
woojung <gnujoow.kim@gmail.com>
2016-09-17 16:36:44 +0900

    countdown

--- src/components/CountDown/CountDown.js
+++ src/components/CountDown/CountDown.js
@@ -1,24 +1,18 @@
 import React from 'react';
 
+const festival = 'October 22 2016 00:00:00 GMT+0900';
+
 export default class CountDown extends React.Component {
   constructor() {
     super();
 
-    let crnt = new Date();
+    let remain = this.getTimeRemaining(festival);
     this.state = {
-      hour: crnt.getHours(),
-      min : crnt.getMinutes(),
-      sec : crnt.getSeconds()
+      days: remain.days,
+      hours: remain.hours,
+      minutes: remain.minutes,
+      seconds: remain.seconds
     };
-  }
-
-  tick() {
-    let crnt = new Date();
-    this.setState({
-       hour: crnt.getHours(),
-       min : crnt.getMinutes(),
-       sec : crnt.getSeconds()
-     });
   }
 
   componentDidMount() {
@@ -31,10 +25,35 @@ export default class CountDown extends React.Component {
     clearInterval(this.timer);
   }
 
+  tick() {
+    let remain = this.getTimeRemaining(festival);
+    this.setState({
+      days: remain.days,
+      hours: remain.hours,
+      minutes: remain.minutes,
+      seconds: remain.seconds
+    });
+  }
+
+  getTimeRemaining(endtime){
+    let total = Date.parse(endtime) - Date.parse(new Date());
+    let seconds = Math.floor( (total/1000) % 60 );
+    let minutes = Math.floor( (total/1000/60) % 60 );
+    let hours = Math.floor( (total/(1000*60*60)) % 24 );
+    let days = Math.floor( total/(1000*60*60*24) );
+    return {
+      'total' : total,
+      'days' : days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    }
+  }
+
   render() {
     return (
       <div>
-        <h1>{this.state.hour}:{this.state.min}:{this.state.sec}</h1>
+        <h1>{this.state.days}일 {this.state.hours}시간 {this.state.minutes}분 {this.state.seconds}초</h1>
       </div>
     );
   }
